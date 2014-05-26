2b264ab8caeff95ce01d25a201841425c224038e
avlazarov <avlazarov@gmail.com>
2014-05-26 14:33:19 +0200

    Setup rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.fixture_path = ::Rails.root.join 'spec', 'fixtures'
+
+  config.use_transactional_fixtures = true
+  config.order = 'random'
+  config.infer_spec_type_from_file_location!
+end

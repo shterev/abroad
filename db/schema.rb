544175ad3e68bed0a20e315e7e4ba7e85ea1a9ec
avlazarov <avlazarov@gmail.com>
2014-05-26 15:58:28 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526121821) do
+ActiveRecord::Schema.define(version: 20140526135724) do
 
   create_table "users", force: true do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.date   "birthday"
-    t.string "color"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthday"
+    t.string   "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

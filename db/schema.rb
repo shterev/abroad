f2d4790663463a51b0f16015edfb79f6f4ce55e1
avlazarov <avlazarov@gmail.com>
2014-05-27 14:08:12 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526135724) do
+ActiveRecord::Schema.define(version: 20140527120024) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140526135724) do
     t.string   "color"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
 end
